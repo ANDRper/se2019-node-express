a9443f91ba1ec76c0b2f667034b7ffbb93301641
AndreaPeratoner <andrea.peratoner@studenti.unitn.it>
2019-11-11 15:54:33 +0100

    D1/Ex1-2
    Day1/Exercise1: completed!
    Day1/Exercise2: working progress

--- day1/exercises/freq.js
+++ day1/exercises/freq.js
@@ -1,1 +1,33 @@
 /* Implement your solution here */
+// Loading the file system library
+const fs = require('fs');
+
+// File name from the common line params
+const fileName = process.argv[2];
+
+// Accessing the content of the file synchnously
+var array = fs.readFileSync(fileName).toString().split("\n");
+
+//Processing array
+var tipoAnimale = new Array();
+var freq = new Array();
+for (var j=0; j<array.length; j++){
+    freq[j]=0;
+}
+
+for (j=0; j<array.length; j++){
+    if ((tipoAnimale.indexOf(array[j])) == -1){
+        console.log('+++%s', array[j]);
+        var newarray = tipoAnimale.push(array[j]);
+        console.log('---%s', newarray);
+    }else{
+        freq[tipoAnimale.indexOf(array[j])]=freq[tipoAnimale.indexOf(array[j])]+1;
+        //console.log('%d', tipoAnimale.indexOf(array[j]));
+    }
+}
+
+//Write results
+console.log('tag ----------- freq \n');
+for(j=0; j<tipoAnimale.length; j++){
+    console.log('%s ----------- %d \n', tipoAnimale[j], freq[j]);
+}
--- day1/exercises/two-numbers.js
+++ day1/exercises/two-numbers.js
@@ -1,1 +1,6 @@
 
+if (process.argv.length == 4){
+    if ((!isNaN(process.argv[2])) && (!isNaN(process.argv[3]))) {
+        console.log('ok!');
+    }
+}
